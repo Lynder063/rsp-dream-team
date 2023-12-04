19665ff4a8ca12c6c8642896cdcf55d3b4bb4133
Lynder063 <lynderjekokot@gmail.com>
2023-12-04 11:28:05 +0100

    Boostrap import

--- Code/MainPage.php
+++ Code/MainPage.php
@@ -5,6 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Časopis VŠPJ</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
@@ -45,6 +56,33 @@
             </ul>
         </div>
     </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <img class="rounded img_clanek"
+                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <div class="text-center">
+                    <a class="nadpis_clanek">Lorem Ipsum</a>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <img class="rounded img_clanek"
+                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <div class="text-center">
+                    <a class="nadpis_clanek">Lorem Ipsum</a>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <img class="rounded img_clanek"
+                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <div class="text-center">
+                    <a class="nadpis_clanek">Lorem Ipsum</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 </body>
 
