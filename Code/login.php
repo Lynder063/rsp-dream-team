17b3db848e27551c87a1fc667ea2255ae39995c2
Lynder063 <lynderjekokot@gmail.com>
2023-12-04 12:01:21 +0100

    Security update

--- Code/login.php
+++ Code/login.php
@@ -10,96 +10,96 @@ session_start(); // Start the session
 </head>
 
 <style>
-body {
-    background: linear-gradient(to left, #4d4d4d 0%, #333333 35%, #333333 65%, #4d4d4d 100%);
+    body {
+        background: linear-gradient(to left, #4d4d4d 0%, #333333 35%, #333333 65%, #4d4d4d 100%);
 
-}
+    }
 
-form {
-    background-color: rgb(255, 255, 255, 0.1);
-    ;
-    padding: 50px;
-    width: 250px;
-    height: 250px;
-    text-align: center;
-    font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
-    font-size: 20px;
-    color: silver;
-    font-weight: bold;
-    border-radius: 10px;
+    form {
+        background-color: rgb(255, 255, 255, 0.1);
+        ;
+        padding: 50px;
+        width: 250px;
+        height: 250px;
+        text-align: center;
+        font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
+        font-size: 20px;
+        color: silver;
+        font-weight: bold;
+        border-radius: 10px;
 
-}
+    }
 
-form label {
-    display: block;
-    margin: 10px 0;
-}
+    form label {
+        display: block;
+        margin: 10px 0;
+    }
 
-input:hover {
-    background-color: grey;
-}
+    input:hover {
+        background-color: grey;
+    }
 
-input:focus {
-    background-color: grey;
-}
+    input:focus {
+        background-color: grey;
+    }
 
-.login {
-    text-align: center;
-    font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
-    font-size: 40px;
-    color: silver;
-    font-weight: bold;
+    .login {
+        text-align: center;
+        font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
+        font-size: 40px;
+        color: silver;
+        font-weight: bold;
 
-}
+    }
 
-.button {
-    background-color: #222;
-    border-radius: 6px;
-    border-style: none;
-    box-sizing: border-box;
-    color: silver;
-    cursor: pointer;
-    display: inline-block;
-    font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1.5;
-    margin: 0;
-    outline: none;
-    overflow: hidden;
-    padding: 9px 20px 8px;
-    position: relative;
-    text-align: center;
-    text-transform: none;
-    user-select: none;
-    -webkit-user-select: none;
-    touch-action: manipulation;
-    width: 80%;
-}
+    .button {
+        background-color: #222;
+        border-radius: 6px;
+        border-style: none;
+        box-sizing: border-box;
+        color: silver;
+        cursor: pointer;
+        display: inline-block;
+        font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
+        font-size: 16px;
+        font-weight: 700;
+        line-height: 1.5;
+        margin: 0;
+        outline: none;
+        overflow: hidden;
+        padding: 9px 20px 8px;
+        position: relative;
+        text-align: center;
+        text-transform: none;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+        width: 80%;
+    }
 
-.button:hover,
-.button:focus {
-    opacity: .75;
-}
+    .button:hover,
+    .button:focus {
+        opacity: .75;
+    }
 
-.form {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 80vh;
-    margin: 0;
-}
+    .form {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+        margin: 0;
+    }
 
-img {
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    transition: 1s;
-}
+    img {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        transition: 1s;
+    }
 
-img:hover {
-    transform: scale(1.5);
-}
+    img:hover {
+        transform: scale(1.5);
+    }
 </style>
 
 <body>
@@ -148,7 +148,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($hashed_password === $stored_password) {
             $_SESSION['user_id'] = $row['id_uzivatele'];
             // Authentication successful, redirect to MainPage.php
-            header("Location: MainPage.php");
+            header("Location: index.php");
             exit;
         }
     }
