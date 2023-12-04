eae8a42643ca6c51a8a4a9d6c918add7ca795be6
Lynder063 <lynderjekokot@gmail.com>
2023-12-04 21:09:45 +0100

    Renaming links

--- Code/profile_editor.php
+++ Code/profile_editor.php
@@ -2,72 +2,72 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Profile editor</title>
-  <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile editor</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-  <a href="index.php"><img src="home.png" alt=""></a>
-  <div class="main-content-box1   ">
-    <div class="profile-img" id="profile-img">
-      <input type="file" id="upload" accept="image/jpeg, image/png" style="display: none;" hidden>
-      <label>Změnit avatar</label>
-    </div>
-    <div class="content-input-box">
+    <a href="index.php"><img src="home.png" alt=""></a>
+    <div class="main-content-box1   ">
+        <div class="profile-img" id="profile-img">
+            <input type="file" id="upload" accept="image/jpeg, image/png" style="display: none;" hidden>
+            <label>Změnit avatar</label>
+        </div>
+        <div class="content-input-box">
 
-    </div>
-    <div class="change-button" id="changeUsernameBtn">
-      <h3>Uživatelské jméno</h3>
-    </div>
-    <div class="change-button">
-      <h3>Email</h3>
-    </div>
-    <div class="change-button" id="changePasswordBtn">
-      <h3>Heslo</h3>
-    </div>
-
-
-  </div>
-
-  <div id="usernameModal" class="modal1">
-    <div class="modal-content1">
-      <span class="close">&times;</span>
-      <form id="usernameForm">
-        <label for="currentUsername">Uživatelské jméno:</label>
-        <input type="username" id="currentUsername" name="currentUsername" required>
-        <label for="currentPassword">Současné heslo:</label>
-        <input type="password" id="Password" name="currentPassword" required>
-        <input type="submit" value="Změnit">
-      </form>
-    </div>
-  </div>
+        </div>
+        <div class="change-button" id="changeUsernameBtn">
+            <h3>Uživatelské jméno</h3>
+        </div>
+        <div class="change-button">
+            <h3>Email</h3>
+        </div>
+        <div class="change-button" id="changePasswordBtn">
+            <h3>Heslo</h3>
+        </div>
 
 
-
-
-  <div id="passwordModal" class="modal">
-    <div class="modal-content">
-      <span class="close">&times;</span>
-      <form id="passwordForm">
-        <label for="currentPassword">Původní heslo:</label>
-        <input type="password" id="currentPassword" name="currentPassword" required>
-
-        <label for="newPassword">Nové heslo:</label>
-        <input type="password" id="newPassword" name="newPassword" required>
-
-        <label for="confirmPassword">Potvrdit heslo:</label>
-        <input type="password" id="confirmPassword" name="confirmPassword" required>
-
-        <input type="submit" value="Změnit">
-      </form>
     </div>
 
-  </div>
-  <script>
+    <div id="usernameModal" class="modal1">
+        <div class="modal-content1">
+            <span class="close">&times;</span>
+            <form id="usernameForm">
+                <label for="currentUsername">Uživatelské jméno:</label>
+                <input type="username" id="currentUsername" name="currentUsername" required>
+                <label for="currentPassword">Současné heslo:</label>
+                <input type="password" id="Password" name="currentPassword" required>
+                <input type="submit" value="Změnit">
+            </form>
+        </div>
+    </div>
+
+
+
+
+    <div id="passwordModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <form id="passwordForm">
+                <label for="currentPassword">Původní heslo:</label>
+                <input type="password" id="currentPassword" name="currentPassword" required>
+
+                <label for="newPassword">Nové heslo:</label>
+                <input type="password" id="newPassword" name="newPassword" required>
+
+                <label for="confirmPassword">Potvrdit heslo:</label>
+                <input type="password" id="confirmPassword" name="confirmPassword" required>
+
+                <input type="submit" value="Změnit">
+            </form>
+        </div>
+
+    </div>
+    <script>
     document.getElementById('profile-img').addEventListener('click', function() {
-      document.getElementById('upload').click();
+        document.getElementById('upload').click();
     });
 
 
@@ -76,11 +76,11 @@
     var closePasswordModal = document.getElementById("passwordModal").getElementsByClassName("close")[0];
 
     changePasswordBtn.onclick = function() {
-      passwordModal.style.display = "block";
+        passwordModal.style.display = "block";
     }
 
     closePasswordModal.onclick = function() {
-      passwordModal.style.display = "none";
+        passwordModal.style.display = "none";
     }
 
     // When the user clicks on the button, open the modal for changing username
@@ -89,36 +89,36 @@
     var closeUsernameModal = document.getElementById("usernameModal").getElementsByClassName("close")[0];
 
     changeUsernameBtn.onclick = function() {
-      usernameModal.style.display = "block";
+        usernameModal.style.display = "block";
     }
 
     closeUsernameModal.onclick = function() {
-      usernameModal.style.display = "none";
+        usernameModal.style.display = "none";
     }
 
     // When the user clicks anywhere outside of either modal, close it
     window.onclick = function(event) {
-      if (event.target == passwordModal) {
-        passwordModal.style.display = "none";
-      } else if (event.target == usernameModal) {
-        usernameModal.style.display = "none";
-      }
+        if (event.target == passwordModal) {
+            passwordModal.style.display = "none";
+        } else if (event.target == usernameModal) {
+            usernameModal.style.display = "none";
+        }
     }
 
     // Form submission logic for password change
     document.getElementById('passwordForm').onsubmit = function(event) {
-      event.preventDefault();
-      // Insert your logic for changing password here
-      alert('Password change logic not implemented yet.');
+        event.preventDefault();
+        // Insert your logic for changing password here
+        alert('Password change logic not implemented yet.');
     }
 
     // Form submission logic for username change
     document.getElementById('usernameForm').onsubmit = function(event) {
-      event.preventDefault();
-      // Insert your logic for changing username here
-      alert('Username change logic not implemented yet.');
+        event.preventDefault();
+        // Insert your logic for changing username here
+        alert('Username change logic not implemented yet.');
     }
-  </script>
+    </script>
 </body>
 
 </html>
