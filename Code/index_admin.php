f1b4fda65c4ad2de78f5a91f4b3fd342e9daa1c6
Lynder063 <lynderjekokot@gmail.com>
2023-12-04 12:53:11 +0100

    s

--- Code/index_admin.php
+++ Code/index_admin.php
@@ -22,30 +22,25 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Časopis VŠPJ</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
     <style>
-    .img_clanek {
-        height: 200px;
-        width: auto;
-    }
+        .img_clanek {
+            height: 200px;
+            width: auto;
+        }
     </style>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -66,8 +61,7 @@ if (!isset($_SESSION['user_id'])) {
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Menu
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -88,22 +82,19 @@ if (!isset($_SESSION['user_id'])) {
     <div class="container">
         <div class="row">
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
@@ -114,22 +105,19 @@ if (!isset($_SESSION['user_id'])) {
     <div class="container">
         <div class="row">
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
@@ -140,22 +128,19 @@ if (!isset($_SESSION['user_id'])) {
     <div class="container">
         <div class="row">
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
             </div>
             <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek"
-                    src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
+                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
                 <div class="text-center">
                     <a class="nadpis_clanek">Lorem Ipsum</a>
                 </div>
