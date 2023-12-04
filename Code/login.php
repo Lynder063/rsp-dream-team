0fce6493d31be7e943974a512441acb9953b143e
Lynder063 <lynderjekokot@gmail.com>
2023-12-04 11:44:21 +0100

    Option to register

--- Code/login.php
+++ Code/login.php
@@ -10,100 +10,100 @@ session_start(); // Start the session
 </head>
 
 <style>
-    body {
-        background: linear-gradient(to left, #4d4d4d 0%, #333333 35%, #333333 65%, #4d4d4d 100%);
+body {
+    background: linear-gradient(to left, #4d4d4d 0%, #333333 35%, #333333 65%, #4d4d4d 100%);
 
-    }
+}
 
-    form {
-        background-color: rgb(255, 255, 255, 0.1);
-        ;
-        padding: 50px;
-        width: 250px;
-        height: 250px;
-        text-align: center;
-        font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
-        font-size: 20px;
-        color: silver;
-        font-weight: bold;
-        border-radius: 10px;
+form {
+    background-color: rgb(255, 255, 255, 0.1);
+    ;
+    padding: 50px;
+    width: 250px;
+    height: 250px;
+    text-align: center;
+    font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
+    font-size: 20px;
+    color: silver;
+    font-weight: bold;
+    border-radius: 10px;
 
-    }
+}
 
-    form label {
-        display: block;
-        margin: 10px 0;
-    }
+form label {
+    display: block;
+    margin: 10px 0;
+}
 
-    input:hover {
-        background-color: grey;
-    }
+input:hover {
+    background-color: grey;
+}
 
-    input:focus {
-        background-color: grey;
-    }
+input:focus {
+    background-color: grey;
+}
 
-    .login {
-        text-align: center;
-        font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
-        font-size: 40px;
-        color: silver;
-        font-weight: bold;
+.login {
+    text-align: center;
+    font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
+    font-size: 40px;
+    color: silver;
+    font-weight: bold;
 
-    }
+}
 
-    .button {
-        background-color: #222;
-        border-radius: 6px;
-        border-style: none;
-        box-sizing: border-box;
-        color: silver;
-        cursor: pointer;
-        display: inline-block;
-        font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
-        font-size: 16px;
-        font-weight: 700;
-        line-height: 1.5;
-        margin: 0;
-        outline: none;
-        overflow: hidden;
-        padding: 9px 20px 8px;
-        position: relative;
-        text-align: center;
-        text-transform: none;
-        user-select: none;
-        -webkit-user-select: none;
-        touch-action: manipulation;
-        width: 80%;
-    }
+.button {
+    background-color: #222;
+    border-radius: 6px;
+    border-style: none;
+    box-sizing: border-box;
+    color: silver;
+    cursor: pointer;
+    display: inline-block;
+    font-family: "Farfetch Basis", "Helvetica Neue", Arial, sans-serif;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1.5;
+    margin: 0;
+    outline: none;
+    overflow: hidden;
+    padding: 9px 20px 8px;
+    position: relative;
+    text-align: center;
+    text-transform: none;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    width: 80%;
+}
 
-    .button:hover,
-    .button:focus {
-        opacity: .75;
-    }
+.button:hover,
+.button:focus {
+    opacity: .75;
+}
 
-    .form {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 80vh;
-        margin: 0;
-    }
+.form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80vh;
+    margin: 0;
+}
 
-    img {
-        position: absolute;
-        top: 20px;
-        right: 20px;
-        transition: 1s;
-    }
+img {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    transition: 1s;
+}
 
-    img:hover {
-        transform: scale(1.5);
-    }
+img:hover {
+    transform: scale(1.5);
+}
 </style>
 
 <body>
-    <a href="MainPage.php"><img src="home.png" alt=""></a>
+    <a href="index.php"><img src="home.png" alt=""></a>
     <h1 class="login">Přihlášení</h1>
     <div class="form">
         <form method="post">
@@ -115,6 +115,7 @@ session_start(); // Start the session
             <input type="password" id="password" name="password" required><br><br>
             <br>
             <input type="submit" value="Přihlásit" class="button">
+            <a href="register.php">Registrace</a>
         </form>
     </div>
 </body>
