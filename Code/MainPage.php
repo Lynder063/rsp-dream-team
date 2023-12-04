6f8362faffcad6f7021e1a3fbe51155a3006d44e
Lynder063 <lynderjekokot@gmail.com>
2023-12-04 11:29:29 +0100

    Image temp

--- Code/MainPage.php
+++ Code/MainPage.php
@@ -16,6 +16,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
+    <style>
+    .img_clanek {
+        height: 200px;
+        width: auto;
+    }
+    </style>
 </head>
 
 <body>
@@ -82,6 +88,7 @@
             </div>
         </div>
     </div>
+
 
 
 </body>
