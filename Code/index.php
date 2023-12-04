1092d6e9133681facad648e28da54086d5a56c84
Lynder063 <lynderjekokot@gmail.com>
2023-12-04 14:03:58 +0100

    Importing DB record as a article

--- Code/index.php
+++ Code/index.php
@@ -1,18 +1,18 @@
 <?php
-
-// checking if user is logged in
-
+// checking if the user is logged in
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 session_start();
+require_once('db.php');
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
 
-
+// Fetch data from the Clanek table
+$sql = "SELECT nazev_clanku, img_url FROM Clanek";
+$result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
@@ -22,25 +22,30 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Časopis VŠPJ</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
     <style>
-        .img_clanek {
-            height: 200px;
-            width: auto;
-        }
+    .img_clanek {
+        height: 200px;
+        width: auto;
+    }
     </style>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -61,7 +66,8 @@ if (!isset($_SESSION['user_id'])) {
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Menu
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -74,74 +80,44 @@ if (!isset($_SESSION['user_id'])) {
         </div>
     </nav>
 
-    <!-- ROW 1 -->
     <div class="container">
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- ROW 2 -->
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- ROW 3 -->
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <img class="rounded img_clanek" src="https://i0.wp.com/impact.hugoapp.com/wp-content/uploads/2021/08/qi-bin-w4hbafegiac-unsplash.jpg?fit=1600%2C1066&ssl=1">
-                <div class="text-center">
-                    <a class="nadpis_clanek">Lorem Ipsum</a>
-                </div>
-            </div>
-        </div>
+        <?php
+        if ($result->num_rows > 0) {
+            $count = 0;
+
+            // Output rows in groups of 3
+            while ($row = $result->fetch_assoc()) {
+                if ($count % 3 == 0) {
+                    // Close previous row if not the first row
+                    if ($count > 0) {
+                        echo '</div>';
+                    }
+
+                    // Open a new row for every 3 records
+                    echo '<div class="row">';
+                }
+
+                $nazevClanku = $row['nazev_clanku'];
+                $imgUrl = $row['img_url'];
+
+                // Output the HTML for each record
+                echo '<div class="col-md-4 mb-4">';
+                echo '<img class="rounded img_clanek" src="' . $imgUrl . '">';
+                echo '<div class="text-center">';
+                echo '<a class="nadpis_clanek">' . $nazevClanku . '</a>';
+                echo '</div>';
+                echo '</div>';
+
+                $count++;
+            }
+
+            // Close the last row
+            echo '</div>';
+        } else {
+            // Handle case when no rows are found
+            echo '<p>No articles found.</p>';
+        }
+        ?>
     </div>
 </body>
 
