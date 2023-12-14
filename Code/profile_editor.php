4ca363de1484ef085664b59ffb39012bd550571d
webspaghetti <ondrakohondra@email.cz>
2023-12-14 20:52:36 +0100

    Add logic for profile_editor.php

--- Code/profile_editor.php
+++ Code/profile_editor.php
@@ -1,3 +1,131 @@
+<?php
+session_start();
+require_once('db.php');
+
+// Je uživatel přihlášen ?
+if (!isset($_SESSION['user_id'])) {
+    // Jestli ne jde na login
+    header("Location: login.php");
+    exit();
+}
+
+// Žádný blbý znaky
+function sanitize_input($data) {
+    return htmlspecialchars(strip_tags($data));
+}
+
+// Změna uživatelskýho jména
+if (isset($_POST['changeUsernameBtn'])) {
+    $oldUsername = sanitize_input($_POST['oldUsername']);
+    $newUsername = sanitize_input($_POST['newUsername']);
+    $userId = $_SESSION['user_id'];
+
+    // Starý uživatelský jméno
+    $query = "SELECT uzivatelske_jmeno FROM Uzivatel WHERE id='$userId'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $oldUsernameFromDB = $row['uzivatelske_jmeno'];
+
+        // Podívej se jestli hodnoty matchují s databází
+        if ($oldUsername === $oldUsernameFromDB) {
+            // Update
+            $sql = "UPDATE Uzivatel SET uzivatelske_jmeno='$newUsername' WHERE id='$userId'";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Uživatelské jméno změněno";
+            } else {
+                echo "Error při updatu uživatelského jména: " . $conn->error;
+            }
+        } else {
+            echo "Staré uživatelské jméno není správné";
+        }
+    } else {
+        echo "Error při získávání údajů o uživateli: " . $conn->error;
+    }
+}
+
+
+// Změna emailu
+if (isset($_POST['changeEmailBtn'])) {
+    $oldEmail = sanitize_input($_POST['oldEmail']);
+    $newEmail = sanitize_input($_POST['newEmail']);
+    $userId = $_SESSION['user_id'];
+
+    // Starý email
+    $query = "SELECT email_adresa FROM Uzivatel WHERE id='$userId'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $oldEmailFromDB = $row['email_adresa'];
+
+        // Podívej se jestli zadaný email matchuje
+        if ($oldEmail === $oldEmailFromDB) {
+            // Update
+            $sql = "UPDATE Uzivatel SET email_adresa='$newEmail' WHERE id='$userId'";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Email změněn";
+            } else {
+                echo "Error při updatu emailu: " . $conn->error;
+            }
+        } else {
+            echo "Starý email není správný";
+        }
+    } else {
+        echo "Error při získávání údajů o uživateli: " . $conn->error;
+    }
+}
+
+// Změna hesla
+if (isset($_POST['changePasswordBtn'])) {
+    $oldPasswordInput = sanitize_input($_POST['oldPassword']);
+    $newPassword = sanitize_input($_POST['newPassword']);
+    $newPasswordAgain = sanitize_input($_POST['newPasswordAgain']);
+    $userId = $_SESSION['user_id'];
+
+    // Nezadal uživatel stejná hesla ?
+    if ($newPassword !== $newPasswordAgain) {
+        echo "Nová hesla se neshodují.";
+    } else {
+        // Staré heslo a salt
+        $query = "SELECT heslo, salt FROM Uzivatel WHERE id='$userId'";
+        $result = $conn->query($query);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $oldHashedPassword = hash('sha256', $oldPasswordInput . $row['salt']);
+
+            // Podívej se jestli zadané heslo matchuje
+            if ($oldHashedPassword === $row['heslo']) {
+                // Nová sůl
+                $newSalt = bin2hex(random_bytes(16));
+                $newHashedPassword = hash('sha256', $newPassword . $newSalt);
+
+                // Update
+                $sql = "UPDATE Uzivatel SET heslo='$newHashedPassword', salt='$newSalt' WHERE id='$userId'";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "Heslo změněno";
+                } else {
+                    echo "Error při změně hesla: " . $conn->error;
+                }
+            } else {
+                echo "Staré heslo není správné";
+            }
+        } else {
+            echo "Error při získávání údajů o uživateli: " . $conn->error;
+        }
+    }
+}
+
+
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,114 +139,39 @@
 <body>
     <a href="index.php"><img src="home.png" alt=""></a>
     <div class="main-content-box1   ">
-        <div class="profile-img" id="profile-img">
-            <input type="file" id="upload" accept="image/jpeg, image/png" style="display: none;" hidden>
-            <label>Změnit avatar</label>
-        </div>
-        <div class="content-input-box">
-
-        </div>
         <div class="change-button" id="changeUsernameBtn">
             <h3>Uživatelské jméno</h3>
+            <form method="post">
+                <label for="oldUsername">Staré uživatelské jméno:</label>
+                <input type="text" name="oldUsername" required>
+                <label for="newUsername">Nové uživatelské jméno:</label>
+                <input type="text" name="newUsername" required>
+                <button type="submit" name="changeUsernameBtn">Změnit</button>
+            </form>
         </div>
         <div class="change-button">
             <h3>Email</h3>
+            <form method="post">
+                <label for="oldEmail">Starý email:</label>
+                <input type="email" name="oldEmail" required>
+                <label for="newEmail">Nový email:</label>
+                <input type="email" name="newEmail" required>
+                <button type="submit" name="changeEmailBtn">Změnit</button>
+            </form>
         </div>
         <div class="change-button" id="changePasswordBtn">
             <h3>Heslo</h3>
-        </div>
-
-
-    </div>
-
-    <div id="usernameModal" class="modal1">
-        <div class="modal-content1">
-            <span class="close">&times;</span>
-            <form id="usernameForm">
-                <label for="currentUsername">Uživatelské jméno:</label>
-                <input type="username" id="currentUsername" name="currentUsername" required>
-                <label for="currentPassword">Současné heslo:</label>
-                <input type="password" id="Password" name="currentPassword" required>
-                <input type="submit" value="Změnit">
-            </form>
-        </div>
-    </div>
-
-
-
-
-    <div id="passwordModal" class="modal">
-        <div class="modal-content">
-            <span class="close">&times;</span>
-            <form id="passwordForm">
-                <label for="currentPassword">Původní heslo:</label>
-                <input type="password" id="currentPassword" name="currentPassword" required>
-
+            <form method="post">
+                <label for="oldPassword">Staré heslo:</label>
+                <input type="password" name="oldPassword" required>
                 <label for="newPassword">Nové heslo:</label>
-                <input type="password" id="newPassword" name="newPassword" required>
-
-                <label for="confirmPassword">Potvrdit heslo:</label>
-                <input type="password" id="confirmPassword" name="confirmPassword" required>
-
-                <input type="submit" value="Změnit">
+                <input type="password" name="newPassword" required>
+                <label for="newPasswordAgain">Nové heslo:</label>
+                <input type="password" name="newPasswordAgain" required>
+                <button type="submit" name="changePasswordBtn">Změnit</button>
             </form>
         </div>
-
     </div>
-    <script>
-    document.getElementById('profile-img').addEventListener('click', function() {
-        document.getElementById('upload').click();
-    });
-
-
-    var passwordModal = document.getElementById("passwordModal");
-    var changePasswordBtn = document.getElementById("changePasswordBtn");
-    var closePasswordModal = document.getElementById("passwordModal").getElementsByClassName("close")[0];
-
-    changePasswordBtn.onclick = function() {
-        passwordModal.style.display = "block";
-    }
-
-    closePasswordModal.onclick = function() {
-        passwordModal.style.display = "none";
-    }
-
-    // When the user clicks on the button, open the modal for changing username
-    var usernameModal = document.getElementById("usernameModal");
-    var changeUsernameBtn = document.getElementById("changeUsernameBtn");
-    var closeUsernameModal = document.getElementById("usernameModal").getElementsByClassName("close")[0];
-
-    changeUsernameBtn.onclick = function() {
-        usernameModal.style.display = "block";
-    }
-
-    closeUsernameModal.onclick = function() {
-        usernameModal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of either modal, close it
-    window.onclick = function(event) {
-        if (event.target == passwordModal) {
-            passwordModal.style.display = "none";
-        } else if (event.target == usernameModal) {
-            usernameModal.style.display = "none";
-        }
-    }
-
-    // Form submission logic for password change
-    document.getElementById('passwordForm').onsubmit = function(event) {
-        event.preventDefault();
-        // Insert your logic for changing password here
-        alert('Password change logic not implemented yet.');
-    }
-
-    // Form submission logic for username change
-    document.getElementById('usernameForm').onsubmit = function(event) {
-        event.preventDefault();
-        // Insert your logic for changing username here
-        alert('Username change logic not implemented yet.');
-    }
-    </script>
 </body>
 
 </html>
