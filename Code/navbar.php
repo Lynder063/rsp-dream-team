a513906933a20842133c865031295a5439a23074
Kryštof Malinda <56117532+Lynder063@users.noreply.github.com>
2023-12-22 12:44:28 +0100

    Update navbar.php
    L

--- Code/navbar.php
+++ Code/navbar.php
@@ -15,7 +15,7 @@
     </script>
 
 </head>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark"><a href="index.php"><img src="home.png" height="45px"
+<nav class="navbar navbar-expand-lg navbar-light bg-light"><a href="index.php"><img src="home.png" height="45px"
                                                                                     width="45px"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
